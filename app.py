43117b63e2659776af20696ac5bf5faa9aafb4ea
yltyadi <adi.yeltay@nu.edu.kz>
2023-10-19 20:52:32 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,126 @@
+import requests
+import json
+import telebot
+from telebot import types
+
+import config
+
+word_limit = 5
+cur_offset = 1
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    btn1 = types.KeyboardButton("List of words")
+    btn2 = types.KeyboardButton("Get one word")
+    btn3 = types.KeyboardButton("test")
+    markup.add(btn1, btn2, btn3)
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Hello, {message.from_user.first_name}!",
+        reply_markup=markup,
+    )
+
+
+@bot.message_handler(commands=["parasite_words"])
+def list_parasite_words(message):
+    markup = types.InlineKeyboardMarkup()
+    prev_btn = types.InlineKeyboardButton("Previous", callback_data="prev_page")
+    next_btn = types.InlineKeyboardButton("Next", callback_data="next_page")
+    markup.row(prev_btn, next_btn)
+
+    parasite_words = requests.get(
+        f"http://duryssoile.nu.edu.kz/api/v1.0/words?type=parasite&offset={cur_offset}&limit={word_limit}&sort=asc"
+    )
+    if parasite_words.status_code != 200:
+        bot.send_message(
+            message.chat.id, f"Error! {parasite_words.status_code} Status Code recieved"
+        )
+        return
+
+    result = ""
+    for word in json.loads(parasite_words.text):
+        result += word["word"] + " -> " + word["correctVersions"][0]["word"] + "\n"
+
+    bot.send_message(message.chat.id, result, reply_markup=markup)
+
+
+# have to move it another func to reset variable
+@bot.message_handler(commands=["mispronounced_words"])
+def list_mispronounced_words(message):
+    parasite_words = requests.get(
+        f"http://duryssoile.nu.edu.kz/api/v1.0/words?type=parasite&offset={cur_offset}&limit={word_limit}&sort=asc"
+    )
+    if parasite_words.status_code != 200:
+        bot.send_message(
+            message.chat.id, f"Error! {parasite_words.status_code} Status Code recieved"
+        )
+        return
+
+    markup = types.InlineKeyboardMarkup(row_width=2)
+
+    for word in json.loads(parasite_words.text):
+        btn = types.InlineKeyboardButton(word["word"], callback_data=word["id"])
+        markup.add(btn)
+
+    if cur_offset != 1:
+        prev_btn = types.InlineKeyboardButton("⏪ Previous", callback_data="prev_page")
+    next_btn = types.InlineKeyboardButton("Next ⏩", callback_data="next_page")
+    if "prev_btn" in locals():
+        markup.row(prev_btn, next_btn)
+    else:
+        markup.row(next_btn)
+
+    bot.send_message(message.chat.id, "List of words:", reply_markup=markup)
+
+
+"""
+@bot.message_handler(content_types=["text"])
+def main(message):
+    result = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={config.WEATHER_API}&units=metric"
+    )
+    if result.status_code == 200:
+        data = json.loads(result.text)
+        temp = data["main"]["temp"]
+        bot.send_message(
+            chat_id=message.chat.id,
+            text=f"Temperature in city {message.text} is {data}",
+        )
+    else:
+        bot.send_message(chat_id=message.chat.id, text="Error occured")
+"""
+
+
+@bot.message_handler()
+def main(message):
+    bot.send_message(chat_id=message.chat.id, text="Cannot understand you(")
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_page_handler(callback):
+    global cur_offset
+    if callback.data == "prev_page":
+        cur_offset -= 1
+        bot.delete_message(
+            chat_id=callback.message.chat.id, message_id=callback.message.message_id
+        )
+        list_mispronounced_words(callback.message)
+    elif callback.data == "next_page":
+        # bot.send_message(callback.message.chat.id, callback.message)
+        cur_offset += 1
+        bot.delete_message(
+            chat_id=callback.message.chat.id, message_id=callback.message.message_id
+        )
+        list_mispronounced_words(callback.message)
+        # bot.edit_message_text(repl)
+
+
+if __name__ == "__main__":
+    bot.polling(non_stop=True)
+
+# bot.reply_to(message=callback.message, text="next page")
+# bot.register_next_step_handler(callback.message, list_parasite_words)
