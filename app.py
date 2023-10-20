1c2ea99b2be096b08327d171cd7c8338a764631f
yltyadi <adi.yeltay@nu.edu.kz>
2023-10-20 17:51:00 +0600

    feat: added /mispronounced_words command

--- app.py
+++ app.py
@@ -1,58 +1,42 @@
+# libraries
 import requests
 import json
 import telebot
 from telebot import types
 
+# local imports
 import config
 
 word_limit = 5
-cur_offset = 1
+parasite_cur_offset = 1
+mispronounced_cur_offset = 1
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    btn1 = types.KeyboardButton("List of words")
-    btn2 = types.KeyboardButton("Get one word")
-    btn3 = types.KeyboardButton("test")
-    markup.add(btn1, btn2, btn3)
+    # markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    # btn1 = types.KeyboardButton("List of words")
+    # btn2 = types.KeyboardButton("Get one word")
+    # btn3 = types.KeyboardButton("test")
+    # markup.add(btn1, btn2, btn3)
     bot.send_message(
         chat_id=message.chat.id,
         text=f"Hello, {message.from_user.first_name}!",
-        reply_markup=markup,
+        # reply_markup=markup,
     )
 
 
 @bot.message_handler(commands=["parasite_words"])
 def list_parasite_words(message):
-    markup = types.InlineKeyboardMarkup()
-    prev_btn = types.InlineKeyboardButton("Previous", callback_data="prev_page")
-    next_btn = types.InlineKeyboardButton("Next", callback_data="next_page")
-    markup.row(prev_btn, next_btn)
+    parasite_cur_offset = 1
+    parasite_words_helper(parasite_cur_offset, message)
 
+
+def parasite_words_helper(offset, message):
     parasite_words = requests.get(
-        f"http://duryssoile.nu.edu.kz/api/v1.0/words?type=parasite&offset={cur_offset}&limit={word_limit}&sort=asc"
-    )
-    if parasite_words.status_code != 200:
-        bot.send_message(
-            message.chat.id, f"Error! {parasite_words.status_code} Status Code recieved"
-        )
-        return
-
-    result = ""
-    for word in json.loads(parasite_words.text):
-        result += word["word"] + " -> " + word["correctVersions"][0]["word"] + "\n"
-
-    bot.send_message(message.chat.id, result, reply_markup=markup)
-
-
-# have to move it another func to reset variable
-@bot.message_handler(commands=["mispronounced_words"])
-def list_mispronounced_words(message):
-    parasite_words = requests.get(
-        f"http://duryssoile.nu.edu.kz/api/v1.0/words?type=parasite&offset={cur_offset}&limit={word_limit}&sort=asc"
+        f"http://duryssoile.nu.edu.kz/api/v1.0/words?type=parasite&offset={offset}&limit={word_limit}&sort=asc"
     )
     if parasite_words.status_code != 200:
         bot.send_message(
@@ -66,9 +50,47 @@ def list_mispronounced_words(message):
         btn = types.InlineKeyboardButton(word["word"], callback_data=word["id"])
         markup.add(btn)
 
-    if cur_offset != 1:
-        prev_btn = types.InlineKeyboardButton("⏪ Previous", callback_data="prev_page")
-    next_btn = types.InlineKeyboardButton("Next ⏩", callback_data="next_page")
+    if offset != 1:
+        prev_btn = types.InlineKeyboardButton(
+            "⏪ Previous", callback_data="parasite_prev_page"
+        )
+    next_btn = types.InlineKeyboardButton("Next ⏩", callback_data="parasite_next_page")
+    if "prev_btn" in locals():
+        markup.row(prev_btn, next_btn)
+    else:
+        markup.row(next_btn)
+
+    bot.send_message(message.chat.id, "List of words:", reply_markup=markup)
+
+
+@bot.message_handler(commands=["mispronounced_words"])
+def list_mispronounced_words(message):
+    mispronounced_cur_offset = 1
+    mispronounced_words_helper(mispronounced_cur_offset, message)
+
+
+def mispronounced_words_helper(offset, message):
+    misspronounced_words = requests.get(
+        f"http://duryssoile.nu.edu.kz/api/v1.0/words?type=commonly-mispronounced&offset={offset}&limit={word_limit}&sort=asc"
+    )
+    if misspronounced_words.status_code != 200:
+        bot.send_message(
+            message.chat.id,
+            f"Error! {misspronounced_words.status_code} Status Code recieved",
+        )
+        return
+
+    markup = types.InlineKeyboardMarkup(row_width=2)
+
+    for word in json.loads(misspronounced_words.text):
+        btn = types.InlineKeyboardButton(word["word"], callback_data=word["id"])
+        markup.add(btn)
+
+    if offset != 1:
+        prev_btn = types.InlineKeyboardButton(
+            "⏪ Previous", callback_data="misspro_prev_page"
+        )
+    next_btn = types.InlineKeyboardButton("Next ⏩", callback_data="misspro_next_page")
     if "prev_btn" in locals():
         markup.row(prev_btn, next_btn)
     else:
@@ -102,21 +124,35 @@ def main(message):
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_page_handler(callback):
-    global cur_offset
-    if callback.data == "prev_page":
-        cur_offset -= 1
+    global parasite_cur_offset
+    if callback.data == "parasite_prev_page":
+        parasite_cur_offset -= 1
         bot.delete_message(
             chat_id=callback.message.chat.id, message_id=callback.message.message_id
         )
-        list_mispronounced_words(callback.message)
-    elif callback.data == "next_page":
+        parasite_words_helper(parasite_cur_offset, callback.message)
+    elif callback.data == "parasite_next_page":
         # bot.send_message(callback.message.chat.id, callback.message)
-        cur_offset += 1
+        parasite_cur_offset += 1
         bot.delete_message(
             chat_id=callback.message.chat.id, message_id=callback.message.message_id
         )
-        list_mispronounced_words(callback.message)
+        parasite_words_helper(parasite_cur_offset, callback.message)
         # bot.edit_message_text(repl)
+
+    global mispronounced_cur_offset
+    if callback.data == "misspro_prev_page":
+        mispronounced_cur_offset -= 1
+        bot.delete_message(
+            chat_id=callback.message.chat.id, message_id=callback.message.message_id
+        )
+        mispronounced_words_helper(mispronounced_cur_offset, callback.message)
+    elif callback.data == "misspro_next_page":
+        mispronounced_cur_offset += 1
+        bot.delete_message(
+            chat_id=callback.message.chat.id, message_id=callback.message.message_id
+        )
+        mispronounced_words_helper(mispronounced_cur_offset, callback.message)
 
 
 if __name__ == "__main__":
